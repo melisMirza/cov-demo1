066832a2264028c76f6a62d301836d9ad57c4348
melismirza@gmail.com <melismirza@gmail.com>
2021-01-10 17:05:57 +0300

    22

--- covdemo/settings.py
+++ covdemo/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'covdemo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR + '/db.sqlite3',
     }
 }
 
@@ -115,6 +115,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
